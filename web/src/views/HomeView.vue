39b80d70af3127521e6c354acf5ede0faaf92b78
keiko233 <keiko23333@gmail.com>
2023-06-19 04:43:51 +0800

    refactor: Refactor code to optimize reuse using v-for

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -8,62 +8,16 @@
       </v-card>
     </v-col>
 
-    <v-col cols="12" sm="4">
-      <v-card :title="$t('home.board_info')">
+    <v-col cols="12" sm="4" v-for="card in cards" :key="card.title">
+      <v-card :title="$t(card.title)">
         <template v-slot:text>
-          <Loading v-if="!status" />
-          <div v-else>
-            <p><span>{{ $t('home.board_name') }}: </span>{{ status.system.model }}</p>
-            <p><span>{{ $t('home.manufacturer') }}: </span>{{ status.system.arch.manufacturer }}</p>
-            <p><span>{{ $t('home.board_number') }}: </span>{{ status.system.arch.model }}</p>
-          </div>
-        </template>
-      </v-card>
-    </v-col>
-
-    <v-col cols="12" sm="4">
-      <v-card :title="$t('home.system_info')">
-        <template v-slot:text>
-          <Loading v-if="!status" />
-          <div v-else>
-            <p><span>{{ $t('home.firmware_version') }}: </span>{{ status.system.firmware }}</p>
-            <p><span>{{ $t('home.esp_sdk_version') }}: </span>{{ status.system.esp_sdk }}</p>
-          </div>
-        </template>
-      </v-card>
-    </v-col>
-
-    <v-col cols="12" sm="4">
-      <v-card :title="$t('home.resource_details')">
-        <template v-slot:text>
-          <Loading v-if="!status" />
-          <div v-else>
-            <p><span>{{ $t('home.cpu_frequency') }}: </span>{{ status.system.cpu_freq }} Mhz</p>
-            <p><span>{{ $t('home.available_memory') }}: </span>{{ status.memory.free }}</p>
-          </div>
-        </template>
-      </v-card>
-    </v-col>
-
-    <v-col cols="12" sm="4">
-      <v-card :title="$t('home.access_point_info')">
-        <template v-slot:text>
-          <Loading v-if="!status" />
-          <div v-else>
-            <p><span>{{ $t('home.ssid') }}: </span>{{ status.access_point.ssid }}</p>
-            <p><span>{{ $t('home.connected_num') }}: </span>{{ status.access_point.num }}</p>
-          </div>
-        </template>
-      </v-card>
-    </v-col>
-
-    <v-col cols="12" sm="4">
-      <v-card :title="$t('home.static_access_point_info')">
-        <template v-slot:text>
-          <Loading v-if="!status" />
-          <div v-else>
-            <p><span>{{ $t('home.static_ssid') }}: </span>{{ status.static_access_point.ssid }}</p>
-            <p><span>{{ $t('home.status') }}: </span>{{ status.static_access_point.status }}</p>
+          <v-overlay v-model="loading" contained class="align-center justify-center">
+            <Loading v-if="loading" />
+          </v-overlay>
+          <div>
+            <p v-for="item in card.items" :key="item.label">
+              <span>{{ $t(item.label) }}: </span>{{ getStatusValue(item.key) }}
+            </p>
           </div>
         </template>
       </v-card>
@@ -74,17 +28,72 @@
 <script setup lang="ts">
 const env = ref(import.meta.env.VITE_ENV);
 
-const status = ref();
+const systemInfo = ref();
+
+const loading = ref(false);
+
+const cards = [
+  {
+    title: 'home.board_info',
+    items: [
+      { label: 'home.board_name', key: 'system.model' },
+      { label: 'home.manufacturer', key: 'system.arch.manufacturer' },
+      { label: 'home.board_number', key: 'system.arch.model' }
+    ]
+  },
+  {
+    title: 'home.system_info',
+    items: [
+      { label: 'home.firmware_version', key: 'system.firmware' },
+      { label: 'home.esp_sdk_version', key: 'system.esp_sdk' }
+    ]
+  },
+  {
+    title: 'home.resource_details',
+    items: [
+      { label: 'home.cpu_frequency', key: 'system.cpu_freq' },
+      { label: 'home.available_memory', key: 'memory.free' }
+    ]
+  },
+  {
+    title: 'home.access_point_info',
+    items: [
+      { label: 'home.ssid', key: 'access_point.ssid' },
+      { label: 'home.connected_num', key: 'access_point.num' }
+    ]
+  },
+  {
+    title: 'home.static_access_point_info',
+    items: [
+      { label: 'home.static_ssid', key: 'static_access_point.ssid' },
+      { label: 'home.status', key: 'static_access_point.status' }
+    ]
+  }
+];
 
 const getStatus = () => {
-  console.log(import.meta.env.VITE_API_URL + '/status');
+  loading.value = true;
   fetch('/api/status')
     .then(response => response.json())
     .then(response => {
-      status.value = response;
-      console.log(status.value);
+      loading.value = false;
+      systemInfo.value = response;
+      console.log(systemInfo.value);
     })
     .catch(err => console.error(err));
+};
+
+const getStatusValue = (key: string) => {
+  const keys = key.split('.');
+  let value = systemInfo.value;
+  for (const k of keys) {
+    if (value && value.hasOwnProperty(k)) {
+      value = value[k];
+    } else {
+      return '';
+    }
+  }
+  return value;
 };
 
 onMounted(() => {
