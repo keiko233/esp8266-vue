576acaeac3c3e6dc2f055b76d8e8c68ad4553140
keiko233 <keiko23333@gmail.com>
2023-06-19 23:52:57 +0800

    style: Migrate SettingsShortcuts to single file

---
+++ web/src/components/SettingsShortcuts/SettingsShortcuts.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card :title="$t('settings.shortcuts')">
+    <template v-slot:text>
+      <div>
+        <v-btn @click="softReboot" block class="mb-2">{{ $t('settings.soft_reboot') }}</v-btn>
+        <v-btn @click="restart" block>{{ $t('settings.restart') }}</v-btn>
+      </div>
+    </template>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const softReboot = () => {
+  fetch('/api/softreboot')
+};
+
+const restart = () => {
+  fetch('/api/restart')
+};
+</script>
