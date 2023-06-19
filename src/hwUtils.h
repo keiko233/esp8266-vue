d35630074a6b74cd2de37c01e139f4589bb47f9e
keiko233 <keiko23333@gmail.com>
2023-06-19 23:47:58 +0800

    feat: set Serial Timeout Duration in Database

--- src/hwUtils.h
+++ src/hwUtils.h
@@ -1,14 +1,12 @@
 #ifndef HW_UTILS_H
 #define HW_UTILS_H
 
-#define SERIAL_TIMEOUT_DURATION 100
-
 #include "main.h"
 
 void controlLed(int ledPin, int duration, bool shouldBlink = false, int blinkInterval = 500);
 void asyncLedLoop();
-float getHumidity(unsigned long timeoutDuration = SERIAL_TIMEOUT_DURATION);
-float getTemperature(unsigned long timeoutDuration = SERIAL_TIMEOUT_DURATION);
-int getPhotoresistance(unsigned long timeoutDuration = SERIAL_TIMEOUT_DURATION);
+float getHumidity(unsigned long timeoutDuration = strtoul(findDatabase("serial_timeout_duration").c_str(), nullptr, 10));
+float getTemperature(unsigned long timeoutDuration = strtoul(findDatabase("serial_timeout_duration").c_str(), nullptr, 10));
+int getPhotoresistance(unsigned long timeoutDuration = strtoul(findDatabase("serial_timeout_duration").c_str(), nullptr, 10));
 
 #endif
